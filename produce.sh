4c2ebbcb99f352405740379ec2f561017a4bd008
julien Gaffuri <julien.gaffuri@gmail.com>
2016-08-15 14:23:08 +0200

    a

--- produce.sh
+++ produce.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-#TODO
+size=1000
+margin=5
+mkdir -p tmp
+mkdir -p json
+topojson -o \
+	"json/communes.json" \
+	"communes=shp/COMMUNE.shp" \
+	"limites=shp/LIMITE_COMMUNE.shp" \
+	-p id=INSEE_COM,nom=NOM_COM,st=STATUT,na=NATURE \
+	--width $size --height $size --margin $margin \
+	-s 2
