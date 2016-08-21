5ba551e4146c5e1fe818a42f7d55683e92ccecaa
julien Gaffuri <julien.gaffuri@gmail.com>
2016-08-22 00:06:18 +0200

    new version

--- produce.sh
+++ produce.sh
@@ -2,15 +2,25 @@
 
 size=1000
 margin=5
+q=10000
+s=0.5
 mkdir -p tmp
 mkdir -p json
+
+echo "   communes surf + lin"
 topojson -o \
 	"json/communes.json" \
 	"communes=shp/COMMUNE.shp" \
 	"limites=shp/LIMITE_COMMUNE.shp" \
 	-p id=INSEE_COM,nom=NOM_COM,st=STATUT_C,na=NATURE_C \
 	--width $size --height $size --margin $margin \
-	-s 0.5
-	#-q0 10000 \
-	#-q1 10000 \
-	#
+	-s $s -q $q
+	#-q0 100000 -q1 100000
+
+echo "   communes surf"
+topojson -o \
+	"json/communes_.json" \
+	"communes=shp/COMMUNE.shp" \
+	-p id=INSEE_COM,nom=NOM_COM,st=STATUT_C,na=NATURE_C \
+	--width $size --height $size --margin $margin \
+	-s $s -q $q
